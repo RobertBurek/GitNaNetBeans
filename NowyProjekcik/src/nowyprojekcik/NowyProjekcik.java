758c7c63df90995764df7ef26c611ecc1bb11c49
Robert Burek <robertburek@wp.pl>
2019-03-28 16:51:17 +0100

    Dodałem trzecią totalnie nie potrzebną linię tekstu.
        	 brawo Robercie, dodajesz coraz to nowe linijki teksstu, jestem pod wrażeniem Twojej pracy
    	może czas póść trochę do przodu i napisać jakiś okinkowy interfejs do swojego projektu
    	na przyklad w  JavaFX. Do dzieła.

--- NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
+++ NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
@@ -5,11 +5,14 @@ package nowyprojekcik;
  * @author Robert Burek
  */
 public class NowyProjekcik {
-
+   
+   int i;
 
    public static void main(String[] args) {
+      
       System.out.println("Coś nowego na ekranie.");
       System.out.println("Kolejna linia tekstu.");
+      System.out.println("Czaaaas na trzecią linijką.  HAHAHAH.");
    }
    
 }
