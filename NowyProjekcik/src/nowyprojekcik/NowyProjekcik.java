bf89a73248ac20311c5f01c286570fa9d944d4b5
Robert Burek <robertburek@wp.pl>
2019-03-28 15:49:20 +0100

    Dodałem kolejna linijke tekstu - druga

--- NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
+++ NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
@@ -9,6 +9,7 @@ public class NowyProjekcik {
 
    public static void main(String[] args) {
       System.out.println("Coś nowego na ekranie.");
+      System.out.println("Kolejna linia tekstu.");
    }
    
 }
