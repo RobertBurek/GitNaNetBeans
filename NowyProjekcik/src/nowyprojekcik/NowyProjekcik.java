c2c27cd8df1eb4b9d38a4c0c3af44af675394a2f
Robert Burek <robertburek@wp.pl>
2019-03-28 17:40:19 +0100

    Wykonałe zadanie n1 1

--- NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
+++ NowyProjekcik/src/nowyprojekcik/NowyProjekcik.java
@@ -5,14 +5,13 @@ package nowyprojekcik;
  * @author Robert Burek
  */
 public class NowyProjekcik {
-   
+
    int i;
 
    public static void main(String[] args) {
-      
+
       System.out.println("Coś nowego na ekranie.");
-      System.out.println("Kolejna linia tekstu.");
       System.out.println("Czaaaas na trzecią linijką.  HAHAHAH.");
    }
-   
+
 }
